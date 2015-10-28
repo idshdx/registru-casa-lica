56a388298e8614c3bf39a7995168f2fd85ac5dfb
zer0lis <idzer0lis@gmail.com>
2015-10-28 20:49:13 +0200

    trebuie testat

--- application/controllers/Registru.php
+++ application/controllers/Registru.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Main extends CI_Controller {
+class Registru extends CI_Controller {
 
 	public function __construct() {
           parent::__construct();
